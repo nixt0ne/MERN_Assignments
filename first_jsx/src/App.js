85b76dfff5a2f6c6a68cf195d54e7f56670bf36f
nstone <nicholasdavidstone@gmail.com>
2022-09-02 20:04:53 -0700

    First JSX

---
+++ first_jsx/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Firstcomp from './Components/Firstcomp';
+
+function App() {
+  return (
+    <div className="App">
+    <Firstcomp/>
+    </div>
+  );
+}
+
+export default App;
