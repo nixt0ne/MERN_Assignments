75e338d4c3e263a3dc3427ec62bafdbca5efb5ef
nstone <nicholasdavidstone@gmail.com>
2022-09-21 14:03:11 -0700

    Pokemon API AXIOS W_btn

--- pokemon_api_axio/src/App.js
+++ pokemon_api_axio/src/App.js
@@ -8,20 +8,28 @@ function App() {
 const [state,setState] = useState([])   
 
 
-  useEffect(()=> {
-    axios.get ('https://pokeapi.co/api/v2/pokemon?limit=807')
-    .then ((res)=>{
-    console.log(res)
-    setState(res.data.results)
-    })
-    .catch((error)=>{
-    })
+  // useEffect(()=> {
 
-    },[])
+  //   .then ((res)=>{
+  //   console.log(res)
+  //   setState(res.data.results)
+  //   })
+  //   .catch((error)=>{
+  //   })
+
+    const makeCall = async () => {
+      try{
+        const results = await axios.get ('https://pokeapi.co/api/v2/pokemon?limit=807')
+        setState (results.data.results)
+      } catch(error) {}
+    }
+
+    // },[])
 
      return (
       
       <div className="App">
+      <button onClick={makeCall}>Make API Call</button>
       {
         state.map((pokemon)=>(
           <div>
